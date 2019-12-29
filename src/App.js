110c4c39b3c529ae5ab6a2e4e3a2d7319ec25ad0
locheikh <lo.cheikh92110@gmail.com>
2019-12-29 14:33:27 +0100

    Création d'un textarea modifiable

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import { render } from 'react-dom';
 import {sampleText } from './sampleText'
 
 class App extends Component {
+  state= {
+    text: sampleText
+  }
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({text})
+  }
+  
   render(){
     return (
       <div className="container">
         <div className="row"> 
           <div className="col-sm-6"> {/* grille 12 collonne mais petit écran (sm) passe à 6 */} 
             <textarea 
-            value={sampleText}
+              onChange={this.handleChange}
+              value={this.state.text}
               className='form-control'
               rows="35">
             </textarea>
           </div>
           <div className="col-sm-6">  
               <div> 
-                { sampleText}
+                { this.state.text}
               </div>
           </div>
 
